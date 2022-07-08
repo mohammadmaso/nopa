68188b299469cf8f41fd26c1195e67c28b185e0f
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-09 01:53:42 +0430

    bug fix

--- components/md-renderer/index.tsx
+++ components/md-renderer/index.tsx
@@ -3,18 +3,16 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import MdxComponents from './mdx-components';
-import { roadmapTheme } from '../../styles/theme';
+import { nopaTheme } from '../../styles/theme';
 
 type MdRendererType = {
-  children: React.ReactNode
+  children: React.ReactNode;
 };
 
 export default function MdRenderer(props: MdRendererType) {
   return (
-    <ChakraProvider theme={roadmapTheme} resetCSS>
-      <MDXProvider components={MdxComponents}>
-        {props.children}
-      </MDXProvider>
+    <ChakraProvider theme={nopaTheme} resetCSS>
+      <MDXProvider components={MdxComponents}>{props.children}</MDXProvider>
     </ChakraProvider>
   );
-};
+}
